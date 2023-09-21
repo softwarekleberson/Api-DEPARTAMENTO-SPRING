1c69d2f4caa26109cbe2ea361a86d7f09fa435bb
softwarekleberson <santossilvakleberson@gmail.com>
2023-09-21 11:13:44 -0300

    tratamento de erro 400 feito

--- api/src/main/java/api/rh/api/commun/infra/tratamento/erro/TratadorErro.java
+++ api/src/main/java/api/rh/api/commun/infra/tratamento/erro/TratadorErro.java
@@ -1,6 +1,8 @@
 package api.rh.api.commun.infra.tratamento.erro;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,4 +16,19 @@ public class TratadorErro {
 		return ResponseEntity.notFound().build();
 	}
 	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+		var erros = ex.getFieldErrors();
+		return ResponseEntity.badRequest()
+							 .body(erros.stream()
+							 .map(DadosErroValidacao::new)
+							 .toList());
+												
+	}
+	
+	private record DadosErroValidacao(String nome, String mensagem){
+		public DadosErroValidacao(FieldError erro) {
+			this(erro.getField(), erro.getDefaultMessage());
+		}
+	}
 }
