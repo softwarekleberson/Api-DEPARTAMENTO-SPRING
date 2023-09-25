0e37d2a97ff231eb8b3900e482d3a9d3ea48ce80
softwarekleberson <santossilvakleberson@gmail.com>
2023-09-25 20:45:03 -0300

    adição de validação na entity cargos e alteração na dto cargos: metodo post

--- api/src/main/java/api/rh/api/domain/cargo/entity/Cargo.java
+++ api/src/main/java/api/rh/api/domain/cargo/entity/Cargo.java
@@ -1,6 +1,7 @@
 package api.rh.api.domain.cargo.entity;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 import api.rh.api.domain.cargo.infra.web.dto.post.DadosCadastroCargo;
 import api.rh.api.domain.cargo.infra.web.dto.post.NivelEstagio;
@@ -28,9 +29,17 @@ public class Cargo {
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	public static final int MAXIMO_NOME_LENGTH = 100;
 	private String nome;
+	
+	public static final int MAXIMO_DESCRICAO_LENGTH = 1000;
 	private String descricao;
+	
+	public static final BigDecimal SALARIO_MINIMO = new BigDecimal("1320");
 	private BigDecimal salario_Base;
+	
+	public static final BigDecimal SALARIO_MAXIMO = new BigDecimal("18000");
 	private BigDecimal salario_Maximo;
 	
 	@Enumerated(EnumType.STRING)
@@ -40,13 +49,14 @@ public class Cargo {
 	
 	public Cargo(DadosCadastroCargo dados) {
 		this.ativo = true;
-		this.nome = dados.nome();
-		this.descricao = dados.descricao();
-		this.salario_Base = dados.salarioBase();
-		this.salario_Maximo = dados.salarioMaximo();
-		this.nivel = dados.nivel();
+		
+		setNome(dados.nome());
+		setDescricao(dados.descricao());
+		setSalario_Base(dados.salarioBase());
+		setSalario_Maximo(dados.salarioMaximo());
+		setNivel(dados.nivel());
 	}
-
+	
 	public void atualizarInformacoes(@Valid DadosAtualizarCargo dados) {
 		
 		if (dados.nome() != null) {
@@ -65,5 +75,39 @@ public class Cargo {
 
 	public void excluir() {
 		this.ativo = false;
+	}
+	
+	public void setNome(String nome) {
+		String verificaNome = Objects.requireNonNull(nome, "Nome não deve ser nulo");
+		if(verificaNome.length() > MAXIMO_NOME_LENGTH) {
+			throw new IllegalArgumentException("Nome não deve ser maior que : " + MAXIMO_NOME_LENGTH + " caracteres");
+		}
+		this.nome = nome;
+	}
+	
+	public void setDescricao(String descricao) {
+		String verificaDescricao = Objects.requireNonNull(descricao, "Descrição não deve ser nula");
+		if(verificaDescricao.length() > MAXIMO_DESCRICAO_LENGTH) {
+			throw new IllegalArgumentException("Descrição não deve ser maior que  : " + MAXIMO_DESCRICAO_LENGTH + " caracteres");
+		}
+		this.descricao = descricao;
+	}
+	
+	public void setSalario_Base(BigDecimal salario_Base) {
+		if(SALARIO_MINIMO.compareTo(salario_Base) > 0) {
+			throw new IllegalArgumentException("Salário mínimo menor do que o piso");
+		}
+		this.salario_Base = salario_Base;
+	}
+	
+	public void setSalario_Maximo(BigDecimal salario_Maximo) {
+		if(salario_Maximo.compareTo(SALARIO_MAXIMO) > 0) {
+			throw new IllegalArgumentException("O salário máximo não deve ser maior do que : " + SALARIO_MAXIMO);
+		}
+		this.salario_Maximo = salario_Maximo;
+	}
+	
+	public void setNivel(NivelEstagio nivel) {
+		this.nivel = Objects.requireNonNull(nivel, "Nivel não deve ser nulo");
 	}
 }
--- api/src/main/java/api/rh/api/domain/cargo/infra/web/dto/post/DadosCadastroCargo.java
+++ api/src/main/java/api/rh/api/domain/cargo/infra/web/dto/post/DadosCadastroCargo.java
@@ -1,27 +1,26 @@
 package api.rh.api.domain.cargo.infra.web.dto.post;
-
-
 import java.math.BigDecimal;
 
+import api.rh.api.domain.cargo.entity.Cargo;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record DadosCadastroCargo(
 		
-		@NotBlank
+		@NotBlank(message = "Nome não deve ser nulo ou maior que : " + Cargo.MAXIMO_NOME_LENGTH + " caracteres")
 		String nome,
 		
-		@NotBlank
+		@NotBlank(message = "Descrição não deve ser nulo ou maior que : " + Cargo.MAXIMO_DESCRICAO_LENGTH + " caracteres")
 		String descricao,
 		
-		@Positive(message = "Salario deve ser superior a 0")
+		@Positive(message = "Valor deve ser maior que 0 : ")
 		BigDecimal salarioBase,
 		
-		@Positive(message = "Salario deve ser superior a 0")
+		@Positive(message = "Valor deve ser maior que 0")
 		BigDecimal salarioMaximo,
 		
-		@NotNull
+		@NotNull(message = "Nível não deve ser nulo")
 		NivelEstagio nivel
 		
 		) {
